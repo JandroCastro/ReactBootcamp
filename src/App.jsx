bea954a21fb4c058c86bf3156be6d17aefdbfa1b
Alejandro <alejandrocal@ext.inditex.com>
2023-12-04 21:45:32 +0100

    Estado de la aplicación tras el primer módulo

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import Header from "./components/Header/Header";
+import ProductsSection from "./components/ProductsSection/ProductsSection";
+import Footer from "./components/Footer/Footer";
+import Banner from "./components/Banner/Banner";
+
+function App() {
+  const [filtro, setFiltro] = useState("");
+
+  return (
+    <>
+      <Header onFilterChange={setFiltro} />
+      <Banner />
+      <ProductsSection filtro={filtro} />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+// Banner.jsx
+
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <p className="banner-text">¡20% de descuento para nuevos clientes!</p>
+    </div>
+  );
+};
+
+export default Banner;
